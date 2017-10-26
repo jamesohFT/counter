881ff9c832ad2454277e8135b2fc03c48b844bc0
James O'Hara <>
2017-10-26 11:28:54 +0100

    finished counter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Counter extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      value: 0
+    }
+    this.handleChange = this.handleChange.bind(this);
+
+
+  }
+
+   handleChange (event) {
+    if(event.target.innerHTML === '+'){
+    	this.setState({
+	      value: this.state.value + 1
+	    });
+    }else{
+    	if(this.state.value === 0){
+  			return;
+  		}
+    	this.setState({
+	      value: this.state.value - 1
+	    });
+    }
+  }
+
+  render () {
+    return (
+      <div className="counter">
+        {this.props.title}
+        {": "}
+        <span className="counter-value">{this.state.value}</span>
+        <button onClick={this.handleChange}>+</button>
+        <button onClick={this.handleChange}>-</button>
+
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Counter title = "Counter"/>,
+  document.getElementById('root')
+);
